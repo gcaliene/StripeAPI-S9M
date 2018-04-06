1bfe48fe7419d23e47d6b04e59723399dd3feba6
gerson C <gersoncalienes@gmail.com>
2018-04-06 15:39:52 -0400

    Setting up initial configuration of server

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+
+let server;
+
+function runServer(databaseUrl = DATABASE_URL || "No Database currently being used.", port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(port, () => {
+          console.log(
+            `Time to work! Your app is listening on port ${port} and db at${databaseUrl}`
+          );
+          resolve();
+        })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('I guess this means you are done. Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
